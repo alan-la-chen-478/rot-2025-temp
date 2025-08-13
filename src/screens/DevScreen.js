1ae9d3e36624a1d98f0428237f9c95d54494652f
Alan Chen <alan@alromedia.com>
2025-08-13 08:04:31 -0700

    add update test

--- src/screens/DevScreen.js
+++ src/screens/DevScreen.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import * as Updates from 'expo-updates';
 import Constants from 'expo-constants';
 import {Alert} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
@@ -10,6 +11,7 @@ import Text from '~elements/Text';
 import Button from '~elements/Button';
 import {getDebugInfo} from '~helpers/debug';
 import {getConfig} from '~helpers/app';
+import * as config from '~configs/app';
 
 const DevScreen = ({navigation}) => {
   const extra = Constants.expoConfig.extra;
@@ -19,11 +21,35 @@ const DevScreen = ({navigation}) => {
     (async () => setAppInfo(await getDebugInfo()))();
   });
 
+  const checkForUpdate = async () => {
+    try {
+      const result = await Updates.checkForUpdateAsync();
+
+      if (result.isAvailable) {
+        await Updates.fetchUpdateAsync();
+
+        alert('Update your app for new features and bug fixes', {
+          title: 'App Update Avaliable',
+          cancelText: 'Later',
+          confirmText: 'Update Now',
+          onConfirm: () => Updates.reloadAsync(),
+        });
+      } else {
+        alert('You are already on the latest patch', {title: 'Up to Date', confirmOnly: true});
+      }
+    } catch (error) {
+      alert(`An error occurred: ${error.message}`, {confirmOnly: true});
+    } finally {
+      setChecking(false);
+    }
+  };
+
   return (
     <ScreenHeader headerText="Developer" containerStyle={{padding: 15}} showBack>
       <Text>Dev Screen</Text>
-      <Button onPress={triggerJsError}>Test Error</Button>
+      <Button onPress={checkForUpdate}>Check For Update</Button>
       <Text>{JSON.stringify(extra, null, 4)}</Text>
+      <Text>{JSON.stringify(config, null, 4)}</Text>
     </ScreenHeader>
   );
 };
